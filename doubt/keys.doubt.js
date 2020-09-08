fbfa0ca96330a24628efbb090dcee95c900fd77e
Ronan <liruonan@hotmail.com>
2020-09-08 22:12:06 +0800

    update(doubt):

--- doubt/keys.doubt.js
+++ doubt/keys.doubt.js
@@ -1,15 +1,29 @@
 import assert from 'assert';
 
 import keys from '../keys';
+import getAllKeys from '.././.internal/getAllKeys'
 
 // 怀疑起因：在keys()源码中区分了arrayLike
-describe('keys', function() {
-  it('keys() 与 Object.keys() 得到的数组集合 应该不相同', function () {
+describe('keys() vs Object.keys(): arguments', function() {
+  it('2者集合子项 应该相同【推断】', function () {
     (() => {
       const result1 = keys(arguments);
       const result2 = Object.keys(arguments);
 
-      assert.notDeepEqual(result1, result2);
+      assert.deepEqual(result1, result2);
     })(1,3,5);
+  });
+});
+
+describe('Object.keys() vs getAllKeys(): Symbol', function() {
+  it('2者集合子项 应该不相同', function () {
+    const obj = Object.create(null);
+    const unikey = Symbol('a');
+    obj[unikey] = 100;
+
+    const result2 = Object.keys(obj);
+    const result3 = getAllKeys(obj);
+
+    assert.notDeepEqual(result3, result2)
   });
 });
